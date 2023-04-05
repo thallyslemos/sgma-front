186b98d38b0572dbcbdbbb95edd89d70174c2a81
Thallys <thallysvilemos@gmail.com>
2023-04-05 16:51:27 -0300

    created homepage dashbord

---
+++ src/components/common/MyCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="q-pa-md">
+    <q-card id="card" class="q-ma-md" :style="`min-height: ${120}px`">
+      <q-card-section class="row bg-grey-2 q-py-sm">
+        <q-icon size="md" :name="props.icon" />
+        <div class="text-h6 q-ml-sm">{{ props.title }}</div>
+      </q-card-section>
+
+      <q-separator />
+      <div class="q-pa-md flex flex-center">
+        {{ props.description }}
+
+        <slot />
+      </div>
+      <div class="q-pa-md flex flex-center">
+        {{ props.content }}
+
+        <slot />
+      </div>
+    </q-card>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: "Titulo",
+  },
+  icon: {
+    type: String,
+    default: "delete",
+  },
+  content: {
+    type: Number,
+    default: null,
+  },
+  description: {
+    type: String,
+    default: "0",
+  },
+});
+</script>
+
+<style scoped>
+#card {
+  width: 100%;
+  max-width: 400px;
+  max-height: 100%;
+  height: 200px;
+  color: #010101;
+  font-weight: bold;
+  transition: 0.3s;
+}
+
+#card:hover {
+  background-color: #00695dc5 !important;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,50 @@
 <template>
-  <q-page class="flex flex-center">teste </q-page>
+  <q-page>
+    <div class="col-12">
+      <div class="row justify-center">
+        <div class="col-md-3">
+          <my-card
+            title="Alunos"
+            icon="school"
+            :content="studdents"
+            description="Alunos Cadastrados"
+            @click="$router.push('/alunos')"
+          />
+        </div>
+        <div class="col-md-3">
+          <my-card
+            title="Cursos"
+            icon="books"
+            :content="courses"
+            description="Cursos Cadastrados"
+            @click="$router.push('/cursos')"
+          />
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import MyCard from "src/components/common/MyCard.vue";
+import coursesService from "src/services/courses";
+import studentsService from "src/services/studenst";
+import { onMounted, ref } from "vue";
+const title = "Título";
+
+// const { list } = coursesService();
+// const { list } = studentsService();
+const courses = ref(null);
+const studdents = ref(null);
+
+onMounted(() => {
+  getData();
+});
+
+const getData = async () => {
+  const studentRes = await studentsService().list();
+  const courseRes = await coursesService().list();
+  courses.value = courseRes.length;
+  studdents.value = studentRes.length;
+};
+</script>
