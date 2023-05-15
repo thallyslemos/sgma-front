0fb24a9cfa955c10b4a8758a6771569886f13fbd
Thallys <thallysvilemos@gmail.com>
2023-05-15 17:14:21 -0300

    aling component center

--- src/components/common/ConectionError.vue
+++ src/components/common/ConectionError.vue
@@ -4,13 +4,15 @@
     enter-active-class="animated fadeIn"
     leave-active-class="animated fadeOut"
   >
-    <div class="q-pa-md q-gutter-lg" v-if="props.show">
-      <q-banner class="bg-grey-4 text-dark shadow-6">
-        <q-icon name="cloud_off" size="4em" />
-        Houve um erro ao carregar os dados dos servidor...
+    <div class="row justify-center" hidden>
+      <div class="q-pa-md q-gutter-lg items-center" v-if="props.show">
+        <q-banner class="bg-grey-4 text-dark shadow-6 BANNER">
+          <q-icon name="cloud_off" size="4em" />
+          Houve um erro ao carregar os dados dos servidor...
 
-        <q-spinner-gears size="4em" />
-      </q-banner>
+          <q-spinner-gears size="4em" />
+        </q-banner>
+      </div>
     </div>
   </transition>
 </template>
@@ -22,3 +24,8 @@ const props = defineProps({
   },
 });
 </script>
+<style scoped>
+.BANNER {
+  max-width: 500px;
+}
+</style>
