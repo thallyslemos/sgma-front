b37522562d42d65564cdbb9d50c5a1f9bec08224
Thallys <thallysvilemos@gmail.com>
2023-05-15 17:18:30 -0300

    created new functions

--- src/stores/auth-store.js
+++ src/stores/auth-store.js
@@ -2,48 +2,39 @@ import { defineStore } from "pinia";
 import { api } from "boot/axios";
 import { ref } from "vue";
 
-export const useAuthStore = defineStore(
-  "auth",
-  () => {
-    const token = ref(sessionStorage.getItem("token"));
-    const user = ref(sessionStorage.getItem("user"));
+export const useAuthStore = defineStore("auth", () => {
+  const token = ref(sessionStorage.getItem("token"));
+  const user = ref(sessionStorage.getItem("user"));
 
-    function setToken(tokenValue) {
-      sessionStorage.setItem("token", tokenValue);
-      token.value = tokenValue;
-      api.defaults.headers.common.Authorization = "Bearer " + tokenValue;
-    }
-
-    function setUser(userValue) {
-      sessionStorage.setItem("user", JSON.stringify(userValue));
-      user.value = userValue;
-    }
-
-    return {
-      setToken,
-      setUser,
-    };
+  function setToken(tokenValue) {
+    sessionStorage.setItem("token", tokenValue);
+    token.value = tokenValue;
+    setBearerToken(tokenValue);
   }
-  // {
-  //   state: () => ({
-  //     authUser: null,
-  //   }),
-  //   getters: {
-  //     user: (user) => state.authUser,
-  //   },
-  //   actions: {
-  //     async getToken() {},
-  //     async login(user, password) {
-  //       let payload = {
-  //         username: `${user}`,
-  //         password: `${password}`,
-  //       };
-  //       await api.post("auth/login", payload).then((response) => {
-  //         const { access_token } = response.data;
-  //         api.defaults.headers.common.Authorization = "Bearer " + access_token;
-  //         window.sessionStorage.setItem("token", JSON.stringify(access_token));
-  //       });
-  //     },
-  //   },
-  // }
-);
+
+  function setBearerToken(token) {
+    api.defaults.headers.common.Authorization = "Bearer " + token;
+  }
+
+  function getToken() {
+    return token.value;
+  }
+
+  function setUser(userValue) {
+    sessionStorage.setItem("user", JSON.stringify(userValue));
+    user.value = userValue;
+  }
+
+  function singOut() {
+    setToken(null);
+    setUser(null);
+  }
+
+  return {
+    setToken,
+    setUser,
+    getToken,
+    setBearerToken,
+    singOut,
+  };
+});
