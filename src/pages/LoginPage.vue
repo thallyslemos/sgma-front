863e7c567d0f83f9d9ee22f2de04b8f347fde7c5
Thallys <thallysvilemos@gmail.com>
2023-05-16 17:11:56 -0300

    fixed form

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -11,43 +11,45 @@
           <h2 class="text-weight-bolder text-primary">SGMA</h2>
         </q-card-section>
 
-        <q-card-section class="q-gutter-sm">
-          <q-input
-            dense
-            v-model="credentials.username"
-            label="Login"
-            lazy-rules
-            :rules="[
-              (val) =>
-                (val && val.length > 3) || 'Digite um nome de usuário válido',
-            ]"
-          >
-            <template v-slot:append>
-              <q-icon name="person" />
-            </template>
-          </q-input>
-          <q-input
-            :type="isPwd ? 'password' : 'text'"
-            dense
-            v-model="credentials.password"
-            label="Senha"
-            lazy-rules
-            :rules="[
-              (val) => (val && val.length > 3) || 'Digite uma senha válida',
-            ]"
-          >
-            <template v-slot:append>
-              <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
-            </template>
-          </q-input>
-        </q-card-section>
-        <q-card-actions class="q-pa-md">
-          <q-btn color="primary" label="Entrar" @click="login"></q-btn>
-        </q-card-actions>
+        <q-form @submit="login" class="q-gutter-md">
+          <q-card-section class="q-gutter-sm">
+            <q-input
+              dense
+              v-model="credentials.username"
+              label="Login"
+              lazy-rules
+              :rules="[
+                (val) =>
+                  (val && val.length > 3) || 'Digite um nome de usuário válido',
+              ]"
+            >
+              <template v-slot:append>
+                <q-icon name="person" />
+              </template>
+            </q-input>
+            <q-input
+              :type="isPwd ? 'password' : 'text'"
+              dense
+              v-model="credentials.password"
+              label="Senha"
+              lazy-rules
+              :rules="[
+                (val) => (val && val.length > 3) || 'Digite uma senha válida',
+              ]"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
+                />
+              </template>
+            </q-input>
+          </q-card-section>
+          <q-card-actions class="q-pa-md justify-center">
+            <q-btn color="primary" label="Entrar" type="submit"></q-btn>
+          </q-card-actions>
+        </q-form>
       </q-card>
     </transition>
   </q-page>
@@ -79,12 +81,10 @@ const login = async () => {
     //   credentials.value.password
     // );
 
-    router.push({ path: "/home" });
+    router.replace({ path: "/home" });
   } catch (e) {
     $q.notify({ message: e.message, icon: "warning", color: "negative" });
   }
-
-  (credentials.value.user = ""), (credentials.value.password = "");
 };
 </script>
 
@@ -93,6 +93,7 @@ const login = async () => {
   display: flex;
   flex-direction: column;
   align-items: center;
+  max-width: 280px;
 }
 #loginView {
   background: radial-gradient(circle, #26a69a 0%, #014a88 100%);
